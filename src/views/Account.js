b58088b5b953b8ae92d6cef1ed1060257327b999
Devon <devon.meza@yahoo.com>
2021-08-17 09:12:47 -0700

    create skeleton pages

---
+++ src/views/Account.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Account = () => {
+  return <div>Welcome to the Account page</div>;
+};
+
+export default Account;
---
+++ src/views/Signup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Signup = () => {
+  return <div>Welcome to the Signup page</div>;
+};
+
+export default Signup;
